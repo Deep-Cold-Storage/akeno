c28adf4a910e8d07bcf4c77ec13673bd1a0d09c6
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2020-05-27 21:35:29 +0200

    Add demo of SQLite integration via EF
    Basic list application with database persistence and EF integration.

--- akeno/akeno/MainWindow.xaml.cs
+++ akeno/akeno/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Diagnostics;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -23,6 +24,69 @@ namespace akeno
         public MainWindow()
         {
             InitializeComponent();
+
+            UpdateMovieList();
+
+        }
+
+        private void UpdateMovieList()
+        {
+            using (var db = new DatabaseContext())
+            {
+                // Create
+                //Trace.WriteLine("Inserting a new movie!");
+                //db.Add(new Movie { Title = "Bee Movie " });
+                //db.SaveChanges();
+
+                // Read
+                //Trace.WriteLine("Querying for a movies!");
+                //var movie = db.Movies.First();
+
+                // Update
+                //Trace.WriteLine("Updating the movie and adding a movie!");
+                //movie.Title = "Bee Movie 2";
+                //db.SaveChanges();
+
+                moviesListBox.ItemsSource = db.Movies.ToList();
+            }
+        }
+
+        private void movieAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new DatabaseContext())
+            {
+     
+                if (movieTitleForm.Text == "")
+                {
+                    return;
+                }
+
+                db.Add(new Movie { Title = movieTitleForm.Text });
+                db.SaveChanges();
+                movieTitleForm.Text = "";
+
+                UpdateMovieList();
+
+
+            }
+        }
+
+        private void movieDeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new DatabaseContext())
+            {
+                var movie = moviesListBox.SelectedItem as Movie;
+                if (movie == null)
+                {
+                    return;
+                }
+
+                Trace.WriteLine(movie.Title);
+
+                db.Remove(movie);
+                db.SaveChanges();
+                UpdateMovieList();
+            }
         }
     }
 }
---
+++ akeno/akeno/Model.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace akeno
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=database.db");
+    }
+
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+    }
+}
